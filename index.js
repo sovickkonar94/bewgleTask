c8a2311cc629882c34a9d4a38d31cdf267ba9229
sovickkonar94 <sovickkonar@gmail.com>
2021-01-12 10:54:07 +0530

    database connected to server

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/constants');
+const { connect } = require('./commons/services/mongoosedb');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-
+connect();
 app.listen(PORT,()=>console.log(`server is listening on http://localhost:${PORT}`));
