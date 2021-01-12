3226c4ae8710f5a54c900b2a777f497a99ac889c
sovickkonar94 <sovickkonar@gmail.com>
2021-01-12 15:38:01 +0530

    cors added base route added

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-
+const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/constants');
 const { connect } = require('./commons/services/mongoosedb');
 
+const base = require('./routes/base');
+
 const app = express();
 
+app.use(cors())
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+
+app.use('/api',base);
 
 connect();
 app.listen(PORT,()=>console.log(`server is listening on http://localhost:${PORT}`));
